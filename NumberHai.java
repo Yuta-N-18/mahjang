38101aae9d4e4bd95201d81c4775339dd8cfb504
Yuta-N-18 <r202304726py@jindai.jp>
2025-05-20 16:38:01 +0900

    Changed types of suits and types from String to int. Changed test program to input using array.

--- NumberHai.java
+++ NumberHai.java
@@ -1,9 +1,9 @@
 public class NumberHai extends Hai {// 数牌クラスの定義
     private int number;// 数字
-    private String suit;// スート
+    private int suit;// スート 1:萬子、2:筒子、3:索子
     private boolean isRed;// 赤ドラかどうか
 
-    NumberHai(int id, boolean isDora, int number, String suit, boolean isRed) {
+    NumberHai(int id, boolean isDora, int number, int suit, boolean isRed) {
         super(id, isDora);
         this.number = number;
         this.suit = suit;
@@ -16,7 +16,7 @@ public class NumberHai extends Hai {// 数牌クラスの定義
         return this.number;
     }
 
-    String getSuit() {// スートを取得
+    int getSuit() {// スートを取得
         return this.suit;
     }
 
@@ -24,22 +24,9 @@ public class NumberHai extends Hai {// 数牌クラスの定義
         return this.isRed;
     }
 
-    int suitInt() {// スートを整数に変換するメソッド
-        switch (this.suit) {
-            case "Man":
-                return 1;
-            case "Pin":
-                return 2;
-            case "Sou":
-                return 3;
-            default:
-                return 0; // 未知のタイプの場合は0を返す
-        }
-    }
-
     int setWeight() {// 重みをセットするメソッド
         int weight = 0;
-        weight += suitInt() * 100 + this.number * 10;
+        weight += getSuit() * 100 + this.number * 10;
         if (this.isRed) {
             weight += 2;
         } else if (this.getDora()) {
@@ -51,7 +38,7 @@ public class NumberHai extends Hai {// 数牌クラスの定義
     boolean isEqual(Hai hai) {// 同じ牌かどうかを判定するメソッド
         if (hai instanceof NumberHai) {// 数牌の場合
             NumberHai numberHai = (NumberHai) hai;// キャスト用のインスタンスnumberHaiを作成
-            return this.number == numberHai.getNumber() && this.suit.equals(numberHai.getSuit());
+            return this.number == numberHai.getNumber() && this.suit == numberHai.getSuit();
         }
         return false;
     }
